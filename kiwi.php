7c961a5d22a5411632b12c47389126302bd69d21
Carlo Alberto Ferraris <github@cafxx.strayorange.com>
2011-12-10 13:20:32 +0100

    Cosmetics to kiwi.php

--- kiwi.php
+++ kiwi.php
@@ -8,7 +8,7 @@ define('BASE_HREF', 'http://www.example.com');
 define('PASSWORD', '<type your password here>');
 $storage = new file_storage("data");
 
-$p = isset($_GET['p']) ? $_GET['p'] : 'Home';
+$p = key_exists('p', $_GET) ? $_GET['p'] : 'Home';
 	
 switch ($p) {
 	case 'Sitemap':
@@ -31,22 +31,22 @@ function create_internal_link($match) {
 	if ($match == 'Home') $link = '/';
 	if (is_invalid_page($match))
 		return $match;
-	else
-		return "<a href=\"$link\" title=\"".get_meta_description($storage->get($match), 120)."\">$match</a>";
+	$description = get_meta_description($storage->get($match), 120);
+	return "<a href=\"$link\" title=\"$description\">$match</a>";
 }
 
-if (isset($_REQUEST['pwd']) && $_REQUEST['pwd'] == PASSWORD) {
+if (key_exists('pwd', $_REQUEST) && $_REQUEST['pwd'] == PASSWORD) {
 	$content = html_entity_decode($_REQUEST['content']);
 	$storage->put($p, $content);
 }
 
 if (!$storage->has($p)) {
 	$newp = $p;
-	$p = "Not found";
-	header("404");
-	$content = "";
-	$content_textarea = "";
-	$content_rendered = "";
+	$p = 'Not found';
+	header('404');
+	$content = '';
+	$content_textarea = '';
+	$content_rendered = '';
 } else {
 	$newp = false;
 	$content = $storage->get($p);
@@ -54,37 +54,28 @@ if (!$storage->has($p)) {
 	$content_rendered = preg_replace_callback('/\{([^\}]+)\}/', 'create_internal_link', $content);
 }
 
-$css_page_id = ereg_replace("[^a-zA-Z0-9]", "", $p);
+$css_page_id = ereg_replace('[^a-zA-Z0-9]', '', $p);
 
 function keyword_extract($page, $text){
-    $text = strtolower(strip_tags($text));
+	$text = strtolower(strip_tags($text));
 	$text = ereg_replace("[^[:alnum:]]+", " ", $text);
-    $commonWords = "about,that's,this,that,than,then,them,there,their,they,it's,with,which,were,where,whose,when,what,her's,he's,have,from,enough";
-    $commonWords = strtolower($commonWords);
-    $words = explode(" ", $text);
+	$commonWords = "about,that's,this,that,than,then,them,there,their,they,it's,with,which,were,where,whose,when,what,her's,he's,have,from,enough";
+	$commonWords = strtolower($commonWords);
+	$words = explode(" ", $text);
 	$words[] = strtolower($page);
-    $commonWords = explode(",", $commonWords);
-    foreach ($words as $value) {
-        $common = false;
-        if (strlen($value) > 3){
-            foreach($commonWords as $commonWord){
-                if ($commonWord == $value){
-                    $common = true;
-                }
-                else{
-                }
-            }
-            if($common != true){
-                $keywords[] = $value;
-            }
-            else{
-            }
-        }
-        else{
-        }
-    }
+	$commonWords = explode(",", $commonWords);
+	foreach ($words as $value) {
+		$common = false;
+		if (strlen($value) > 3) {
+			foreach($commonWords as $commonWord)
+				if ($commonWord == $value)
+					$common = true;
+			if($common != true)
+				$keywords[] = $value;
+		}
+	}
 	$keywords = array_count_values($keywords);
-    arsort($keywords);
+	arsort($keywords);
 	return implode(',', array_slice(array_keys($keywords), 0, 10));
 }
 ?>
@@ -94,13 +85,9 @@ function keyword_extract($page, $text){
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="keywords" content="<?= keyword_extract($p, $content) ?>,CAFxX" />
 <meta http-equiv="description" content="<?= get_meta_description($content, 300) ?>" />
-<title>CAFxXcrossway - <?= $p; ?></title>
+<title>CAFxXcrossway — <?= $p ?></title>
 <link href="CAFxXcrossway.css" rel="stylesheet" type="text/css" media="screen" />
 <link rel="SHORTCUT ICON" href="favicon.ico" />
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
-<!--[if lt IE 8]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script>
-<![endif]-->
 </head>
 
 <body>
@@ -108,9 +95,9 @@ function keyword_extract($page, $text){
 <div id="frame">
 	<form method="post" action="<?= ($newp ? $newp : $p) ?>">
 		<div id="head">
-				<h1><a href="http://cafxx.strayorange.com" rel="start index">CAFxXcrossway</a> - <?= $p . ($newp ? " ($newp)" : "") ?></h1>
-				<div id="controls">
-					<a id="switch" onclick="switchToEditMode();" href="#"><?= ($newp ? "Create" : "Edit") ?></a>
+			<h1><a href="<?= BASE_HREF ?>" rel="start index">CAFxXcrossway</a> — <?= $p ?><?= if ($newp) " ($newp)" ?></h1>
+			<div id="controls">
+				<a id="switch" onclick="switchToEditMode();" href="#"><?= ($newp ? "Create" : "Edit") ?></a>
 				<span id="controlform" style="display:none;">
 					<input type="password" value="<?= $pwd; ?>" name="pwd" style="padding:1px; border-right:none;" /><input type="submit" value="Save" style="padding:0px;" />
 				</span>
@@ -128,6 +115,5 @@ function keyword_extract($page, $text){
 	</form>
 </div>
 <div id="notice">1985-<?= date('Y'); ?> CAFxX</div>
-<script type="text/javascript">_uacct = "UA-1618263-1";	urchinTracker(); </script>
 </body>
 </html>
